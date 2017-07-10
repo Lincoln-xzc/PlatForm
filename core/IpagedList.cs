9abc5edd3c6cb4195b1a7208288050fab92fefb8
Lincoln <xzc524813657@sina.com>
2017-07-10 22:57:40 +0800

    Initial check-in of module Platform

---
+++ core/IpagedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace core
+{
+    public interface IPagedList<T>:IList<T>
+    {
+        int PageIndex { get; set; }
+
+        int PageSize { get; }
+
+        int TotalCount { get; }
+
+        int TotalPages { get; set; }
+
+        bool HasPreviousPage { get; }
+
+        bool HasNextPage { get;}
+    }
+}
---
+++ core/PagedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace core
+{
+    public class PagedList<T> : List<T>, IPagedList<T>
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; }
+
+        public int TotalCount { get; }
+
+        public int TotalPages { get; set;}
+
+        public bool HasPreviousPage { get { return (PageIndex > 0);}}
+
+        public bool HasNextPage { get { return (PageIndex + 1 < TotalPages); } }
+
+        public PagedList(IQueryable<T> source, int  pageIndex, int pageSize)
+        {
+            int total = source.Count();
+            this.TotalCount = total;
+            this.TotalPages = total / pageSize;
+            if (total % pageSize > 0)
+                TotalPages++;
+            this.PageSize = pageSize;
+            this.PageIndex = pageIndex;
+            this.AddRange(source.Skip(pageSize * pageIndex).Take(pageSize).ToList());
+        }
+
+        public PagedList(IList<T> source, int pageIndex, int pageSize){
+            int total = source.Count();
+            this.TotalCount = total;
+            this.TotalPages = total / pageSize;
+            if (total % pageSize > 0)
+                TotalPages++;
+            this.PageSize = pageSize;
+            this.PageIndex = pageIndex;
+            this.AddRange(source.Skip(pageSize * pageIndex).Take(pageSize).ToList());
+        }
+
+        public PagedList(IEnumerable<T> source, int pageIndex, int pageSize){
+            int total = source.Count();
+            this.TotalCount = total;
+            this.TotalPages = TotalCount / pageSize;
+            if (total % pageSize > 0)
+                TotalPages++;
+            this.PageSize = pageSize;
+            this.PageIndex = pageIndex;
+            this.AddRange(source.Skip(pageSize * pageIndex).Take(pageSize).ToList());
+        }
+
+    }
+}
